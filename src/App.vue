7f80c80631e26a533ad7ff31d3de7067f16aa7e0
Wint Thanda Moe <wintthandamoe@gmail.com>
2023-08-22 12:51:40 +0630

    project-setup

--- src/App.vue
+++ src/App.vue
@@ -8,20 +8,10 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
+  max-width: 600px;
+  margin: 0 auto;
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
