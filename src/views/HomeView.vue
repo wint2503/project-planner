f933e9c7278f62aa9dd222cc702aa8856d77e908
Wint Thanda Moe <wintthandamoe@gmail.com>
2023-09-06 23:58:09 +0630

    fetch data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,13 +5,30 @@
 </template>
 
 <script>
-import db from '../firebase/config'
+import { ref } from 'vue';
+// import db from '../firebase/config'
 
 export default {
   name: 'HomeView',
   components: {},
   setup (){
-    
-  }
+    let projects = ref([]);
+    let load = async()=>{
+      try{
+        let res = await fetch("http://localhost:3000/project")
+        if(res.status===404){
+          throw new Error('not found url')
+        }
+        console.log(res)
+
+        
+      }catch(err){
+        console.log(err.message);
+      }
+      
+    }
+    load();
+    return {load, projects}
+}
 }
 </script>
